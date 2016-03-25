a4cb9fb8469fe8a63e29453e9da97933567ae33e
Neil Daftary <nmd278@nyu.edu>
2016-03-24 23:18:49 -0400

    Add fun text.

--- hotness.js
+++ hotness.js
@@ -2,10 +2,15 @@
   //SUP
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
+  window.step = null;
 
   canvas.width = window.innerWidth;
   canvas.height = window.window.innerHeight;
 
+  ctx.font = "48px sans-serif";
+  ctx.fillText("Move your mouse!", canvas.width/2 - 200, canvas.height/2);
+
+  // Thanks SO K3N!
   function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
@@ -19,8 +24,16 @@
     posx = pos.x;
     posy = pos.y;
 
-    ctx.fillStyle = 'rgb(200,0,0)';
-    ctx.fillRect(e.clientX, e.clientY, 5, 5);
+    if (!window.step) {
+      window.step = 1;
+      ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    }
+
+    ctx.fillStyle = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255)  + ',' + Math.floor(Math.random() * 255)  + ')';
+    ctx.fillText("Awwwwwwww Yeahhhhhhh!", canvas.width/2 - 275, canvas.height/2);
+    ctx.fillStyle = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255)  + ',' + Math.floor(Math.random() * 255)  + ')';
+    ctx.fillRect(e.clientX, e.clientY, Math.floor(Math.random() * 75), Math.floor(Math.random() * 75));
     requestAnimationFrame(draw);
   }
 
