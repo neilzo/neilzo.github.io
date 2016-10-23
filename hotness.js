87abdad26ec957dd7b13e70e927229cb345632a4
Neil Daftary <nmd278@nyu.edu>
2016-10-22 23:04:25 -0400

    Add Retina support. Begin new arch for future hotness.

--- hotness.js
+++ hotness.js
@@ -1,14 +1,41 @@
 (function(window, document) {
     //SUP
-    var canvas = document.getElementById('canvas');
+    var PIXEL_RATIO = (function() {
+        var ctx = document.createElement("canvas").getContext("2d"),
+            dpr = window.devicePixelRatio || 1,
+            bsr = ctx.webkitBackingStorePixelRatio ||
+            ctx.mozBackingStorePixelRatio ||
+            ctx.msBackingStorePixelRatio ||
+            ctx.oBackingStorePixelRatio ||
+            ctx.backingStorePixelRatio || 1;
+
+        return dpr / bsr;
+    })();
+
+    var createHiDPICanvas = function(w, h, ratio) {
+        if (!ratio) {
+            ratio = PIXEL_RATIO;
+        }
+        var can = document.createElement("canvas");
+        can.width = w * ratio;
+        can.height = h * ratio;
+        can.style.width = w + "px";
+        can.style.height = h + "px";
+        can.getContext("2d").setTransform(ratio, 0, 0, ratio, 0, 0);
+        return can;
+    }
+
+    var canvas = createHiDPICanvas(window.innerWidth, window.innerHeight);
+    document.body.appendChild(canvas);
     var ctx = canvas.getContext('2d');
+    var shapes = [];
     window.step = null;
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.window.innerHeight;
+    var width = window.innerWidth;
+    var height = window.innerHeight;
 
     ctx.font = "48px sans-serif";
-    ctx.fillText("Move your mouse!", canvas.width / 2 - 200, canvas.height / 2);
+    ctx.fillText("Move your mouse!", width / 2 - 200, height / 2);
 
     // Thanks SO K3N!
     function getMousePos(canvas, evt) {
@@ -19,9 +46,26 @@
         };
     }
 
-    function randomFilledRect(e) {
+    function filledRect(x, y, width, height, opacity) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.opacity = opacity;
+    }
+
+    function randomColor() {
+        ctx.fillStyle = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' +
+            Math.floor(Math.random() * 255) + ')';
+    }
+
+    filledRect.prototype.draw = function() {
         randomColor();
-        ctx.fillRect(e.clientX, e.clientY, Math.floor(Math.random() * 75), Math.floor(Math.random() * 75));
+    }
+
+    function randFilledRect(e) {
+        strokeColor();
+        ctx.fillRect(e.clientX, e.clientY, Math.floor(Math.random() * 100), Math.floor(Math.random() * 100));
     }
 
     function randEmptyRect(e) {
@@ -30,18 +74,13 @@
     }
 
     function randomShape(e) {
-        var options = [randomFilledRect, randEmptyRect];
+        var options = [randFilledRect, randEmptyRect];
         var randNum = Math.floor(Math.random() * options.length);
-        options[randNum](e);
+        return options[randNum](e);
     }
 
     function strokeColor() {
         ctx.strokeStyle = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' +
-            Math.floor(Math.random() * 255) + ')';
-    }
-
-    function randomColor() {
-        ctx.fillStyle = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' +
             Math.floor(Math.random() * 255) + ')';
     }
 
@@ -57,7 +96,7 @@
 
         ctx.fillStyle = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' +
             Math.floor(Math.random() * 255) + ')';
-        ctx.fillText("Awwwwwwww Yeahhhhhhh!", canvas.width / 2 - 275, canvas.height / 2);
+        ctx.fillText("Awwwwwwww Yeahhhhhhh!", width / 2 - 275, height / 2);
         randomShape(e);
 
         requestAnimationFrame(draw);
